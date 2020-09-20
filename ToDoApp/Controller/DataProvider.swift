f034e828a0756c21bbd2c7c0200b25b3574e1ea0
aikalinin <bratohalo@gmail.com>
2020-09-20 11:56:43 +0300

    Added DataProvider functionality in DataSource extension block

--- ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/Controller/DataProvider.swift
@@ -7,18 +7,29 @@
 
 import UIKit
 
-class DataProvider: NSObject, UITableViewDelegate {
+class DataProvider: NSObject {
+    
+    var taskManager: TaskManager?
+    
+    
+}
+
+extension DataProvider: UITableViewDelegate {
     
 }
 
 extension DataProvider: UITableViewDataSource {
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+        return taskManager?.tasksCount ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        return UITableViewCell()
     }
     
     
---
+++ ToDoAppTests/Controller/DataProviderTests.swift
@@ -0,0 +1,45 @@
+//
+//  DataProviderTests.swift
+//  ToDoAppTests
+//
+//  Created by Антон Калинин on 20.09.2020.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class DataProviderTests: XCTestCase {
+    
+    var sut: DataProvider!
+    var tableView: UITableView!
+
+    override func setUpWithError() throws {
+        sut = DataProvider()
+        tableView = UITableView()
+        tableView.dataSource = sut
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testNumberOfSectionsIsTwo() {
+        let numberOfSections = tableView.numberOfSections
+        
+        XCTAssertEqual(numberOfSections, 2)
+    }
+    
+    func testNumberOfRowsInSectionZeroIsTaskCount() {
+        sut.taskManager = TaskManager()
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+        
+        tableView.reloadData()
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
+    }
+
+}
