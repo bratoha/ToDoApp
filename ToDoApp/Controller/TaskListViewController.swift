ddfd9f14456e70998e74042e24fa758c5d90caea
aikalinin <bratohalo@gmail.com>
2020-09-30 16:22:35 +0300

    Push DetailViewController using NotificationCenter

--- ToDoApp/Controller/TaskListViewController.swift
+++ ToDoApp/Controller/TaskListViewController.swift
@@ -24,11 +24,24 @@ class TaskListViewController: UIViewController {
         super.viewDidLoad()
         let taskManger = TaskManager()
         dataProvider.taskManager = taskManger
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(showDetailsWithNotification), name: NSNotification.Name("DidSelectRowNotification"), object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
+    }
+    
+    @objc func showDetailsWithNotification(withNotification notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let task = userInfo["task"] as? Task,
+              let detailViewController = storyboard?.instantiateViewController(identifier: String(describing: DetailViewController.self)) as? DetailViewController else {
+            fatalError()
+        }
+        
+        detailViewController.task = task
+        navigationController?.pushViewController(detailViewController, animated: true)
     }
     
 }
--- ToDoAppTests/Controller/TaskListViewControllerTests.swift
+++ ToDoAppTests/Controller/TaskListViewControllerTests.swift
@@ -107,6 +107,35 @@ class TaskListViewControllerTests: XCTestCase {
         waitForExpectations(timeout: 1, handler: nil)
     }
     
+    func testSelectedCellNotificationPushesDetailVC() {
+        let mockNavigationController = MockNavigationController(rootViewController: sut)
+        UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow})
+            .first?
+            .rootViewController = mockNavigationController
+        
+        sut.loadViewIfNeeded()
+        let task1 = Task(title: "Foo")
+        let task2 = Task(title: "Bar")
+        sut.dataProvider.taskManager?.add(task: task1)
+        sut.dataProvider.taskManager?.add(task: task2)
+        
+        NotificationCenter.default.post(name: NSNotification.Name("DidSelectRowNotification"), object: self,  userInfo: ["task": task2])
+        
+        guard let detailViewController = mockNavigationController.pushedViewController as? DetailViewController else {
+            XCTFail()
+            return
+        }
+        
+        detailViewController.loadViewIfNeeded()
+        XCTAssertNotNil(detailViewController.titleLabel)
+        XCTAssertTrue(detailViewController.task == task2)
+    }
+    
 }
 
 extension TaskListViewControllerTests {
@@ -119,6 +148,15 @@ extension TaskListViewControllerTests {
             isReloaded = true
         }
     }
-    
-    
+}
+
+extension TaskListViewControllerTests {
+    class MockNavigationController : UINavigationController {
+        var pushedViewController: UIViewController?
+        
+        override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+            pushedViewController = viewController
+            super.pushViewController(viewController, animated: animated)
+        }
+    }
 }
